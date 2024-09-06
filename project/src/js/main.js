8cd9b98dde828867008c106fe904495a0610202d
AbaevB <abaev70@hotmail.com>
2024-09-06 14:17:07 +0700

    test

--- project/src/js/main.js
+++
@@ -1,9 +0,0 @@
- document.addEventListener('DOMContentLoaded', function(){
-  console.log('Hello, World!');
-
-  let fahrToCelsius = (fahr) => {
-    return (fahr - 32) * (5 / 9);
-  };
-
-  console.log(fahrToCelsius(0));
- });
